56a55e5d9a0a778b45f4014f247d269ec7dc548d
mtav <Mike.Taverne@bristol.ac.uk>
2022-09-28 09:08:18 +0100

    first commit

---
+++ TMM/run_examples.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tmm
+from tmm import examples
+
+def main():
+    tmm.examples.sample1()
+    tmm.examples.sample2()
+    tmm.examples.sample3()
+    tmm.examples.sample4()
+    tmm.examples.sample5()
+    tmm.examples.sample6()
+    
+if __name__ == "__main__":
+    main()
