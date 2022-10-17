e07c026e36559e878fbd94c01f40891db90cae7a
mtav <Mike.Taverne@bristol.ac.uk>
2022-10-17 18:27:49 +0100

    "main"/"no main" comparison

---
+++ TMM/main-or-no-main/without-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Import the main TMM function we will use:
+from tmm.tmm_core import coh_tmm
+
+# Import numpy:
+import numpy as np
+
+# Import matplotlib for plotting:
+import matplotlib.pyplot as plt
+
+##### YOUR CODE GOES HERE #####
+# list of layer thicknesses in nm
+d_list = [np.inf, 100, 300, np.inf]
+# list of refractive indices
+n_list = [1, 2.2, 3.3+0.3j, 1]
+# call the coh_tmm function
+ret = coh_tmm('s', n_list, d_list, 0, 700)
+# print the return values obtained:
+print(ret)
