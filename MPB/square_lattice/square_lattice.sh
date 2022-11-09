4c82c3509628c1ab1c9ac360be984ce60895ebb9
mtav <Mike.Taverne@bristol.ac.uk>
2022-11-09 02:38:23 +0000

    Release ready: KD6041 - Module_Handbook - Section 4 - Task 3 - Week 7.pdf
    -Added -r -n options to the square lattice bash script so it can be adapted for the hexagonal lattice more easily.
    -Improved filenames.

--- MPB/square_lattice/square_lattice.sh
+++ MPB/square_lattice/square_lattice.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mpb square_lattice.ctl | tee square_lattice.out
 grep freq square_lattice.out > square_lattice.dat
-# create an image of the primitive unit-cell
-h5topng -o square_lattice-1x1.png -X10 -Y10 square_lattice-epsilon.h5:data
-# create an image 3x3 unit-cells
-mpb-data -m 3 square_lattice-epsilon.h5
+# Create an image of the primitive unit-cell:
+mpb-data -r -n 320 -m 1 square_lattice-epsilon.h5
+h5topng -o square_lattice-1x1.png -X10 -Y10 square_lattice-epsilon.h5:data-new
+# Create an image of 3x3 unit-cells:
+mpb-data -r -n 320 -m 3 square_lattice-epsilon.h5
 h5topng -o square_lattice-3x3.png -X10 -Y10 square_lattice-epsilon.h5:data-new
